845ed331e4145231eed32372c36257a0e712a809
driden <lignacio.loureiro@gmail.com>
2019-05-26 16:40:51 -0300

    AddingTransactionChangesBalance

---
+++ ServiceTests/AccountServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceTests
+{
+    [TestClass]
+    public class AccountServiceTests
+    {
+        [TestMethod]
+        public void AddingTransactionChangesBalance()
+        {
+            // Arrange
+            var account = new Account();
+        }
+    }
+}
